a0b04ae16db8a0b3e38efac6294c2b287e27ad73
steampunkWizard <108240403+SteampunkWizardStudios@users.noreply.github.com>
2025-01-16 14:14:47 +0000

    got graphql api up and running

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import prisma from "./prisma";
 import { buildSchema } from "type-graphql";
-import { resolvers } from "@prisma/generated/type-graphql";
+import { resolvers } from "../../prisma/generated/type-graphql";
 import { NextRequest } from "next/server";
 
 const schema = await buildSchema({
