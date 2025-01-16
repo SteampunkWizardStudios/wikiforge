b2ac3ccb6ef32ddaa617d85c7f5dfbaa0c86ce6c
steampunkWizard <108240403+SteampunkWizardStudios@users.noreply.github.com>
2025-01-16 15:59:22 +0000

    Update Apollo Client URI to include API endpoint

--- src/app/apolloWrapper.tsx
+++ src/app/apolloWrapper.tsx
@@ -12,7 +12,7 @@ import {
 function makeClient() {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: "https://opulent-space-sniffle-9r64prv579ghxq4j-3000.app.github.dev/",
+    uri: "https://opulent-space-sniffle-9r64prv579ghxq4j-3000.app.github.dev/api",
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     // fetchOptions: { cache: "no-store" },
